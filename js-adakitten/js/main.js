e2a95bd0e256e7b8ed63714bb9dd8b28cf2e2770
izaskunsv <isaezvelasco@gmail.com>
2024-02-16 11:25:05 +0100

    filter-search added but not working correctly

--- js-adakitten/js/main.js
+++ js-adakitten/js/main.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const kittenDescOne = document.querySelector('.js-card_descriptionOne').value;
-const kittenDescTwo = document.querySelector('.js-card_descriptionTwo').value;
-const kittenDescThree = document.querySelector(
-  '.js-card_descriptionThree'
-).value;
+// descripciones de los gatos
 
-const input_search_desc = document.querySelector('.js_in_search_desc');
-const descrSearchText = input_search_desc.value;
+const kittenDescOne = document.querySelector('.js-card_descriptionOne');
+const kittenDescTwo = document.querySelector('.js-card_descriptionTwo');
+const kittenDescThree = document.querySelector('.js-card_descriptionThree');
+
 
 const newForm = document.querySelector('.js-new-form');
-newForm.classList.remove('collapsed');
+newForm.classList.add('collapsed');
+
+// llamando a los inputs del HTML
 
 const inputPhoto = document.querySelector('.js-input-photo');
 const inputName = document.querySelector('.js-input-name');
 const inputRace = document.querySelector('.js-input-race');
-const inputDesc = document.querySelector('.js_in_search_desc');
+const inputDesc = document.querySelector('.js_input_search_desc');
 const labelMessageError = document.querySelector('.js-label-error');
 const buttonAdd = document.querySelector('.js-add-button');
 const buttonCancel = document.querySelector('.js-cancel-button');
@@ -27,6 +27,52 @@ const valueDesc = inputDesc.value;
 
 const linkNewFormElement = document.querySelector('.js-cat-form');
 
+
+
+// EJERCICIO FILTRAR GATO el evento click asociado al botón buscar//
+// Llamamos al listado de gatos
+const listElement = document.querySelector('.js-list');
+const kittenOne = document.querySelector('.js-kittenOne');
+const kittenTwo = document.querySelector('.js-kittenTwo');
+const kittenThree = document.querySelector('.js-kittenThree');
+
+
+// PASOS:
+// 1º llamamos al input de la descripción en el HTML
+// 2º escuchamos el dato/valor que se ha escrito en el input descripción
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
+
+// PASOS:
+// Cuando el usuario pulse el botón BUSCAR (filtterKitten) 
+// 1- Si la descripción coincide con alguna palabra del 1er gato, te muestra el primer gato
+// 2- Si la descripción no coincide con el 1er gato, buscará en el segundo gato
+// 3- Si la descripción no coincide con el 2do gato, buscará en el tercer gato
+// 4- si no coincide con ninguna palabra de ninguna descripción, no mostrará ningún gato
+const filterKitten = (event) => {
+  event.preventDefault();
+  if (kittenDescOne.includes(descrSearchText)) {
+    listElement.innerHTML += kittenOne;
+  }
+  if (kittenDescTwo.includes(descrSearchText)) {
+    listElement.innerHTML += kittenTwo;
+  }
+  if (kittenDescThree.includes(descrSearchText)) {
+    listElement.innerHTML += kittenThree;
+  }
+};
+
+// PASOS:
+// 1º llamamos al botón BUSCAR del HTML
+// 2º escuchamos al botón BUSCAR y tiene el evento click
+
+const buttonSearch = document.querySelector('.js-button-search');
+buttonSearch.addEventListener('click', filterKitten);
+
+
+
+// JERCICIO : Que te salga texto de 'parece que te has olvidado algo' si no completas todo
 labelMessageError.classList.add('empty');
 
 buttonAdd.addEventListener('click', (event) => {
@@ -91,80 +137,12 @@ function handleClickNewCatForm(event) {
 
 linkNewFormElement.addEventListener('click', handleClickNewCatForm);
 
-// if( kittenDescOne.includes(descrSearchText) ) {
-//    //añadir classlist y hacer que se oculte con la clase .collapsed del CSS!
-//   }
 
-//   if( kittenDescTwo.includes(descrSearchText) ) {
 
-//   }
 
-//   if( kittenDescThree.includes(descrSearchText) ) {
-//   }
 
-// EJERCICIOS  1 Y 2
-// const list = document.querySelector('.js-list');
-// const kittenImageOne = 'https://dev.adalab.es/gato-siames.webp';
-// const kittenNameOne = 'Anastacio';
-// const kittenDescOne =
-//   'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-// const kittenRaceOne = 'Siamés';
 
-// const kittenOne = `
-// <li class="card">
-// <article>
-//   <img
-//     class="card_img"
-//     src="${kittenImageOne}"/>
-//   <h3 class="card_title">${kittenNameOne.toUpperCase()}</h3>
-//   <h4 class="card_race">${kittenRaceOne}</h4>
-//   <p class="card_description">
-//   ${kittenDescOne}
-//   </p>
-// </article>
-// </li>
-// `;
 
-// const kittenImageTwo = 'https://dev.adalab.es/sphynx-gato.webp';
-// const kittenNameTwo = 'Fiona';
-// const kittenDescTwo =
-//   'Produce fascinación y curiosidad. Éxotico, raro, bello, extraño... hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
-// const kittenRaceTwo = 'Sphynx';
 
-// const kittenTwo = `
-// <li class="card">
-//     <img
-//         class="card_img"
-//        src="${kittenImageTwo}"
-//         alt="sphynx-cat"
-//             />
-//             <h3 class="card_title">${kittenNameTwo.toUpperCase()}</h3>
-//             <h4 class="card_race">${kittenRaceTwo}</h4>
-//             <p class="card_description">
-//             ${kittenDescTwo}
-//             </p>
-//           </li>
-// `;
 
-// const kittenImageThree = 'https://dev.adalab.es/maine-coon-cat.webp';
-// const kittenNameThree = 'Cielo';
-// const kittenDescThree =
-//   ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.'
-// const kittenRaceThree = 'Maine Coon';
 
-// const kittenThree = `
-// <li class="card">
-//     <img
-//         class="card_img"
-//        src="${kittenImageThree}"
-//         alt="sphynx-cat"
-//             />
-//             <h3 class="card_title">${kittenNameThree.toUpperCase()}</h3>
-//             <h4 class="card_race">${kittenRaceThree}</h4>
-//             <p class="card_description">
-//             ${kittenDescThree}
-//             </p>
-//           </li>
-// `;
-
-// list.innerHTML = `${kittenOne} ${kittenTwo} ${kittenThree}`;
